50cf92a2aadd3b30521147d623cd3a6885839fc2
Shaharyar <shaharyar.umrani@royalcyber.com>
2024-08-11 18:13:37 +0500

    main: integrated simple area chart for revenue and expenses.

--- finance-dashboard-client/src/scenes/dashboard/Row2.tsx
+++ finance-dashboard-client/src/scenes/dashboard/Row2.tsx
@@ -5,9 +5,9 @@ type Props = {}
 const Row2 = (props: Props) => {
     return (
         <>
+            <DashboardBox gridArea="d"></DashboardBox>
             <DashboardBox gridArea="e"></DashboardBox>
             <DashboardBox gridArea="f"></DashboardBox>
-            <DashboardBox gridArea="a"></DashboardBox>
         </>
     )
 }
