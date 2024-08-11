533605e0e81a611ed04081652fcda6cf7750333a
Shaharyar <shaharyar.umrani@royalcyber.com>
2024-08-11 20:05:23 +0500

    main: completed implementation for Row 2, pie chart data is hard coded and static.

--- finance-dashboard-client/src/state/types.ts
+++ finance-dashboard-client/src/state/types.ts
@@ -26,5 +26,17 @@ export interface GetKpisResponse {
     totalExpenses: number;
     expensesByCategory: ExpensesByCategory;
     monthlyData: Array<Month>;
-    dailyData: Array<Day>
+    dailyData: Array<Day>;
+    createdAt: string;
+    updatedAt: string
+}
+export interface GetProductResponse {
+    id: string;
+    _id: string;
+    __v: number;
+    price: number;
+    expense: number;
+    transactions: Array<string>;
+    createdAt: string;
+    updatedAt: string
 }
